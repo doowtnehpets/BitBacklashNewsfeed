27c07add81694adf57df2b2bb0d8a3c8175d48ce
Stephen Wood <doowtnehpets@gmail.com>
2018-07-15 07:17:09 -0600

    Initial commit. Brand new project.

---
+++ app/src/main/java/com/example/android/bitbacklashnewsfeed/NewsActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.bitbacklashnewsfeed;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
