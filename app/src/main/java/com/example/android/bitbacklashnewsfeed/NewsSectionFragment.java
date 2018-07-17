7cfffd96bf1b1ca84add6b4ba72bc25d0ca0a35c
doowtnehpets <doowtnehpets@gmail.com>
2018-07-17 16:46:53 -0600

    Added swipe to refresh for the fragments. Still needs some work...

--- app/src/main/java/com/example/android/bitbacklashnewsfeed/NewsSectionFragment.java
+++ app/src/main/java/com/example/android/bitbacklashnewsfeed/NewsSectionFragment.java
@@ -10,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -17,11 +18,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class NewsSectionFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<NewsArticle>> {
 
+    // Swipe refresh layout
+    SwipeRefreshLayout swipeRefreshLayout;
     // The Recycler view
     RecyclerView recyclerView;
     // The Progress Bar
@@ -44,12 +48,18 @@ public class NewsSectionFragment extends Fragment implements LoaderManager.Loade
         // Inflate the view with the news_recycler
         View rootView = inflater.inflate(R.layout.news_recycler, container, false);
 
-        // Grab the news_recyclerview from the layout and set fixed size and set the layout manager
+        // Grab the swipe refresh layout
+        swipeRefreshLayout = rootView.findViewById(R.id.news_recycler_swiperefresh);
+
+        // Grab the news_recyclerview from the layout
         recyclerView = rootView.findViewById(R.id.news_recyclerview);
 
         // Grab the progress bar and empty text view to hide or show those if data loads
         progressBar = rootView.findViewById(R.id.news_recycler_progressbar);
         emptyTextView = rootView.findViewById(R.id.news_recycler_emptyview);
+
+        // Set up swipe refresh
+        setupSwipeRefresh();
 
         // If connected to the internet start loading the articles else show error message
         if(connectedToInternet()) getLoaderManager().initLoader(1, null, this);
@@ -70,8 +80,14 @@ public class NewsSectionFragment extends Fragment implements LoaderManager.Loade
     @Override
     public void onLoadFinished(@NonNull Loader<List<NewsArticle>> loader, List<NewsArticle> newsArticles) {
 
+        // Stop swipe refreshing animation
+        swipeRefreshLayout.setRefreshing(false);
+
         // Hide the progress bar
         progressBar.setVisibility(View.GONE);
+
+        // Hide the error message if it came up
+        emptyTextView.setVisibility(View.GONE);
 
         // Set the recycler adapter if newsArticles isn't null, else show no articles found message
         if(newsArticles != null && !newsArticles.isEmpty()){
@@ -86,7 +102,7 @@ public class NewsSectionFragment extends Fragment implements LoaderManager.Loade
 
     @Override
     public void onLoaderReset(@NonNull Loader<List<NewsArticle>> loader) {
-
+        restartLoader();
     }
 
     /**
@@ -98,5 +114,31 @@ public class NewsSectionFragment extends Fragment implements LoaderManager.Loade
         if(connectivityManager == null) return false;
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         return (networkInfo != null && networkInfo.isConnectedOrConnecting());
+    }
+
+
+    /**
+     * restart the loader if internet is connected
+     */
+    private void restartLoader() {
+        if (connectedToInternet())
+            getLoaderManager().restartLoader(1, null, this);
+
+        else {
+            swipeRefreshLayout.setRefreshing(false);
+            Toast.makeText(getContext(), R.string.no_internet, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Sets up the swipe to refresh
+     */
+    private void setupSwipeRefresh() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if (!getLoaderManager().hasRunningLoaders()) restartLoader();
+            }
+        });
     }
 }
